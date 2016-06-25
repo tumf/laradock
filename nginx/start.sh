127d153276852daa721cc8320f25caded5426dd5
tumf <y.takahara@gmail.com>
2016-06-25 21:01:11 +0700

    Fix sed command

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $AUTH_PASSWORD ]; then
   /usr/bin/htpasswd -b -c /etc/nginx/htpasswd user ${AUTH_PASSWORD}
-  sed -i -e 's/#auth_basic/auth_basic/g' < /etc/nginx/nginx.conf
+  sed -i -e 's/#auth_basic/auth_basic/g' /etc/nginx/nginx.conf
 fi
 nginx
