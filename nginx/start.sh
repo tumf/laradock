5cc8d83d7014b647c7b87c2ab6a0403fa1343d02
tumf <y.takahara@gmail.com>
2016-06-25 20:43:06 +0700

    Use AUTH_PASSWORD
    AUTH_PASSWORD: yourpassword

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ $AUTH_USER_PASSWORD ];then
-  echo "${AUTH_USER_PASSWORD}" > /etc/nginx/htpasswd
+if [ $AUTH_PASSWORD ]; then
+  /usr/bin/htpasswd -b -c /etc/nginx/htpasswd user ${AUTH_PASSWORD}
   sed -i -e 's/#auth_basic/auth_basic/g' < /etc/nginx/nginx.conf
 fi
 nginx
