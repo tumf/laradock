807df80fe7c7bca78a0361155d70daf3b0c58132
tumf <y.takahara@gmail.com>
2016-07-01 15:19:45 +0700

    fix chmod

--- application/start.sh
+++ application/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf /var/www/laravel
 cp -rf /src /var/www/laravel
-chmod 777 /var/www/laravel/storage
+chmod 777 -R /var/www/laravel/storage
+chmod 777 -R /var/www/laravel/bootstrap/cache
